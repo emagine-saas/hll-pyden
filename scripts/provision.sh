c92762e0e3b739c109d4c6a7711b95e54010acea
Jason Rauen <jason.rauen@gmail.com>
2019-02-23 21:34:07 -0500

    Created multi architecture vagrant set up. Added support for single search head and single indexer.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -2,21 +2,25 @@
 
 sudo -u splunk cp /home/vagrant/.bashrc /opt/splunk/.bashrc
 sudo -u splunk cp /home/vagrant/.profile /opt/splunk/.profile
-echo 'alias splunk=/opt/splunk/bin/splunk' | sudo tee -a /opt/splunk/.bashrc > /dev/null
-sudo apt-get install -yq cowsay build-essential openssl libssl-dev zlib1g-dev libncurses5-dev libreadline-dev libgdbm-dev libdb5.3-dev libbz2-dev liblzma-dev libsqlite3-dev libffi-dev tcl-dev tk tk-dev libdb-dev libexpat-dev libpcap-dev libpcre3-dev
-sudo -u splunk tee -a /opt/splunk/etc/system/local/user-seed.conf << 'EOF'
+echo 'alias splunk=/opt/splunk/bin/splunk' | sudo tee -a ${SPLUNK_HOME}/.bashrc > /dev/null
+echo "splunk:${SPLUNK_PASS}" | sudo chpasswd
+sudo apt-get update -y
+sudo apt-get install -yq sshpass build-essential libncurses5-dev libgdbm-dev libdb5.3-dev libbz2-dev liblzma-dev libsqlite3-dev libffi-dev tcl-dev tk tk-dev libdb-dev libexpat-dev libpcap-dev libpcre3-dev
+cat << EOF | sudo -u splunk tee -a ${SPLUNK_HOME}/etc/system/local/user-seed.conf
 [user_info]
 USERNAME = admin
-PASSWORD = changeme
+PASSWORD = ${SPLUNK_PASS}
 EOF
-sudo -u splunk tee -a /opt/splunk/etc/log-local.cfg << 'EOF'
+sudo -u splunk tee -a ${SPLUNK_HOME}/etc/log-local.cfg << 'EOF'
 [python]
 splunk.pyden = DEBUG
 EOF
-sudo -u splunk tee -a /opt/splunk/bin/python_splunk.sh << 'EOF'
+sudo -u splunk tee -a ${SPLUNK_HOME}/bin/python_splunk.sh << 'EOF'
 #!/bin/bash
 "$(dirname "$0")/splunk" cmd python "$@"
 EOF
-sudo -u splunk chmod +x /opt/splunk/bin/python_splunk.sh
-
+sudo -u splunk chmod +x ${SPLUNK_HOME}/bin/python_splunk.sh
+sudo -u splunk touch ${SPLUNK_HOME}/etc/.ui_login
 sudo -i -u splunk ${SPLUNK_BIN} start --accept-license --no-prompt
+
+sudo -i -u splunk ${SPLUNK_BIN} restart
--- scripts/start-splunk.sh
+++ scripts/start-splunk.sh
@@ -2,16 +2,11 @@
 
 status=`sudo -u splunk ${SPLUNK_BIN} status`
 
-#echo "Splunk status is: $status"
+echo "Splunk status is: $status"
 
-# maybe status command can emit status codes instead...
-if [ "$status" == *"not running"* ]; then
+if [[ "$status" == *"not running"* ]]; then
     echo Splunk was not running
     sudo -u splunk ${SPLUNK_BIN} start
 else
     echo Splunk is running
 fi
-IP=$(ifconfig eth1 | sed -ne 's/.*inet \([0-9.]\+\) .*/\1/p')
-cowsay <<MOO
-        Splunk is available: "${IP}:8000"
-MOO
