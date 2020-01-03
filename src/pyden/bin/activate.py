dc441dfa7a0fee9a79153d75514eed1daba8c99b
oab1 <ob@hoptroff.com>
2020-01-03 15:51:53 +0000

    Add pyden env from config feature

--- src/pyden/bin/activate.py
+++ src/pyden/bin/activate.py
@@ -9,6 +9,13 @@ else:
     from importlib import reload
     from io import StringIO
 
+# The pyden-env will need to define:
+#    * PYTHONPATH to be able to exec splunk responses; needs to hold the dir for local pyden installed libraries and splunk site-packages, with correct version of python
+#          os.path.dirname(os.path.dirname(__file__))+ os.sep+ str(os.sep).join(["local", "lib", "venv","timesuite", "lib", "python3.7", "site-packages"])+os.pathsep +
+## Python version is locked here, as we have just installed it, ourselves.  Likewise the venv as installed by us, now.
+#          os.environ['SPLUNK_HOME']+os.sep+"lib"+os.sep+"python3.7"+os.sep+ "site-packages" +os.sep
+#    * SPLUNK_HOME to do normal stuff
+#
 
 pyden_config = ConfigParser()
 if 'PYDEN_CONFIG' in os.environ:
@@ -26,9 +33,9 @@ class ActivationError(Exception):
     pass
 
 
-def activate_venv(environment):
-    if environment in pyden_config.sections():
-        py_exec = os.path.join(os.environ.get("SPLUNK_HOME", ""), pyden_config.get(environment, "executable"))
+def activate_venv(env, confFile):
+    if env in pyden_config.sections():
+        py_exec = os.path.join(os.environ.get("SPLUNK_HOME", ""), pyden_config.get(env, "executable"))
     else:
         raise ActivationError
 
@@ -37,15 +44,26 @@ def activate_venv(environment):
         reload(sys)
         return
 
+    if confFile and os.path.isfile( confFile):
+        cc = ConfigParser()
+        cc.read(confFile) 
+        if 'pyden_env' in cc:
+            forkEnv=cc['pyden_env']
+
+    else:
+        forkEnv={}
     base = os.path.dirname(py_exec)
-    path = base + os.pathsep + os.environ["PATH"]
-    os.execve(py_exec, ['python'] + sys.argv, {"PATH": path, "PYDEN_CONFIG": proc_out})
+    forkEnv['PATH'] = base + os.pathsep + os.environ["PATH"]
+    forkEnv['PYDEN_CONFIG']=proc_out
+
+    os.execve(py_exec, ['python'] + sys.argv, forkEnv)
 
 
-def activate_venv_or_die(env=False):
+def activate_venv_or_die(env=False, confFile=False):
     if not env:
         env = pyden_config.get('default-pys', 'environment')
     try:
-        activate_venv(env)
+        activate_venv(env, confFile)
     except ActivationError:
         sys.exit(1)
+
