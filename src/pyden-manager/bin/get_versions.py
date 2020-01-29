ad9da2e3c67d61da4dae860aa739727cf781db3c
oab1 <ob@hoptroff.com>
2020-01-29 15:24:22 +0000

    Missed this file in the commit earlier

--- src/pyden-manager/bin/get_versions.py
+++ src/pyden-manager/bin/get_versions.py
@@ -5,7 +5,6 @@ import re
 from splunk_logger import setup_logging
 from utils import get_proxies
 
-
 if __name__ == "__main__":
     logger = setup_logging()
     settings = dict()
@@ -20,13 +19,14 @@ if __name__ == "__main__":
     # logger.debug(all_versions)
     compatible_versions = [version for version in all_versions if (version.startswith('2') and version > '2.7') or (
                 version.startswith('3') and version > '3.5')]
+
     # logger.debug(compatible_versions)
     # sometime there are only pre release or release candidates so we need to check each compatible version for release
     for version in compatible_versions:
-        url = download_url.rstrip() + "%s/" % version
+        url = str(download_url, 'utf-8').rstrip() +'/'+ str( version)+"/"
         logger.debug(url)
         r = requests.get(url, headers={'Cache-Control': 'no-cache'}, proxies=proxies)
-        source_pattern = r"""<a href=\"(?P<link>.*)\">Python-%s.tgz""" % version.replace('.', '\\.')
+        source_pattern = r"""<a href=\"(?P<link>.*)\">Python-{}.tgz""".format( version.replace('.', '\\.'))
         # logger.debug(source_pattern)
         # logger.debug(r.text)
         match = re.findall(source_pattern, r.text)
